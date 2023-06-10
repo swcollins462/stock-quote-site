ab0de726f4f9dabb785dc5f74fd464fda1160d67
stephen <swcollins462@gmail.com>
2023-06-10 17:41:27 -0400

    display quote JS

--- stock-quotes.js
+++ stock-quotes.js
@@ -6,6 +6,78 @@ let quoteContainer = document.querySelector('.js-quote-container');
 let searchResultContainer = document.querySelector('.js-name-response')
 let newsContainer = document.querySelector('.js-news-container');
 
+function formatMktCap(mktCap) {
+    if (mktCap > 999999999999) {
+        mktCap = (mktCap / 1e12).toFixed(2) + 'T' //trillion
+    } else if (mktCap > 999999999) {
+        mktCap = (mktCap / 1e9).toFixed(2) + 'B' //billion
+    } else if (mktCap > 999999) {
+        mktCap = (mktCap / 1e6).toFixed(2) + 'M' //million
+    }
+    return mktCap;
+}
+
+function getChangeColor(change) {
+    if (change < 0) {
+        return 'red';
+    } else {
+        return 'green';
+    }
+}
+
+function displayQuote() {
+    let quoteData = testQuote;
+    let quote = quoteData[0][0];
+    let profile = quoteData[1][0];
+    let mktCap = formatMktCap(profile.mktCap);
+    let chgAmt = quote.change.toFixed(2);
+    let chgPct = quote.changesPercentage.toFixed(2);
+    let changeColor = getChangeColor(quote.change);
+    
+    let html = `
+        <h2 class="quote-name">${quote.name}</h2>
+        <p class="exchange-ticker">${quote.exchange}: ${quote.symbol}</p>
+        <p class="quote-price">${quote.price} <span class="currency">${profile.currency}</span></p>
+        <p class="change ${changeColor}">${chgAmt} (${chgPct}%)</p>
+        <div class="table-container"><div class="column"><table>
+        <tr><th class="label">Open</th><td class="value">${quote.open}</td></tr>
+        <tr><th class="label">High</th><td class="value">${quote.dayHigh}</td></tr>
+        <tr><th class="label">Low</th><td class="value">${quote.dayLow}</td></tr></table></div>
+        <div class="column"><table><tr><th class="label">Mkt cap</th><td class="value">${mktCap}</td></tr>
+        <tr><th class="label">P/E ratio</th><td class="value">${quote.pe}</td></tr>
+        <tr><th class="label">EPS</th><td class="value">${quote.eps}</td></tr></table></div>
+        <div class="column"><table><tr><th class="lable">52-wk high</th>
+        <td class="value">${quote.yearHigh}</td></tr>
+        <tr><th class="label">52-wk low</th><td class="value">${quote.yearLow}</td></tr>
+        <tr><th class="label">Sector</th><td class="value">${profile.sector}</td></tr></table></div></div>
+    `;
+    quoteContainer.innerHTML = html;
+    searchResultContainer.innerHTML = '';
+}
+
+function displaySearchResults(searchResults) {
+    let results = searchResults;
+    let html = '<table>'
+
+    for (let result of results) {
+        html += `<tr><td class="value"><a href="#" onclick="return queryTicker('${result.symbol}')">
+            ${result.exchangeShortName}: ${result.symbol}</a></td><td class="value">
+            <a href="#" onclick="return queryTicker('${result.symbol}')">${result.name}</a></td></tr>
+        `
+    }
+    html += '</table>'
+    searchResultContainer.innerHTML = html;
+    quoteContainer.innerHTML = '';
+}
+
+function renderQuote(response) {
+    if (typeof response[0][0] === 'undefined') {
+        displayErrorMessage('quoteContainer');
+    } else {
+        displayQuote(response);
+    }
+}
+
 function getNewsImageHTML(news) {
     if (typeof news.image === 'string') {
         return `<img src="${news.image}" alt="${news.title}" class="news-img col-12 col-md-6 col-lg-4" />`;
@@ -46,7 +118,7 @@ function renderNews(response) {
 }
 
 function queryNews() {
-    const url = `https://financialmodelingprep.com/api/v3/fmp/articles?page=0&size=8&apikey=${apiKey}`
+    const url = `https://financialmodelingprep.com/api/v3/fmp/articles?page=0&size=8&apikey=${process.env.API_KEY}`
 
     fetch(url)
         .then((data) => data.json())
